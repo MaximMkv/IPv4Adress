041b3cc29d21f124d7b357425723112b3e0c5ff3
MaximMkv <maximmahov78@gmail.com>
2023-11-01 21:55:16 +0200

    Create IPv4Adress.rb

---
+++ IPv4Adress.rb
@@ -0,0 +1,19 @@
+def is_ipv4_address?(str)
+  parts = str.split('.')
+
+  return false if parts.length != 4
+
+  parts.each do |part|
+    return false unless part.match(/^\d+$/)  # перевірка що символи є циярами
+    return false if part.to_i < 0 || part.to_i > 255  # перевірка чи числа в межах 0 - 255
+    return false if part.to_i.to_s != part  # перевіряємо, чи немає ведучіх нулів
+  end
+  true
+end
+
+puts is_ipv4_address?("192.168.1.1")  # +
+puts is_ipv4_address?("255.255.255.0")  # +
+puts is_ipv4_address?("0.0.0.0")  # +
+puts is_ipv4_address?("256.0.0.1")  # -
+puts is_ipv4_address?("192.168.01.1")  -
+puts is_ipv4_address?("192.168.1")  # -
