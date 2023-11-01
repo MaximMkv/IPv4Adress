616ee5d8c2be3e16b4dba37816e83066affb0072
MaximMkv <116109934+MaximMkv@users.noreply.github.com>
2023-11-01 21:56:58 +0200

    Update IPv4Adress.rb

--- IPv4Adress.rb
+++ IPv4Adress.rb
@@ -4,7 +4,7 @@ def is_ipv4_address?(str)
   return false if parts.length != 4
 
   parts.each do |part|
-    return false unless part.match(/^\d+$/)  # перевірка що символи є циярами
+    return false unless part.match(/^\d+$/)  # перевірка що символи є цифрами
     return false if part.to_i < 0 || part.to_i > 255  # перевірка чи числа в межах 0 - 255
     return false if part.to_i.to_s != part  # перевіряємо, чи немає ведучіх нулів
   end
